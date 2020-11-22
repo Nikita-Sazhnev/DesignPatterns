bf56e1bbb4ad427e0750bf3c99b656f1b3faaa90
Nikita <nikitas4478@gmail.com>
2020-11-22 19:33:27 +0200

    FactoryMethod

---
+++ FactoryMethod.php
@@ -0,0 +1,51 @@
+<?
+
+abstract class DiscFactory {
+
+    abstract public function createDisc(): DiscInterface;
+}
+
+class HDDFactory extends DiscFactory {
+
+    public function createDisc(): DiscInterface {
+        return new HDD;
+    }
+}
+
+class SSDFactory extends DiscFactory {
+
+    public function createDisc(): DiscInterface {
+        return new SSD;
+    }
+}
+interface DiscInterface {
+    public function readMemory();
+    public function writeMemory();
+}
+
+
+class HDD implements DiscInterface {
+
+    public function readMemory() {
+        echo "Read data from magnet discs";
+    }
+    public function writeMemory() {
+        echo "Write data to memory discs";
+    }
+}
+
+class SSD implements DiscInterface {
+    public function readMemory() {
+        echo "Read data from magnet cells";
+    }
+    public function writeMemory() {
+        echo "Write data to memory cells";
+    }
+}
+
+$creators = [new HDDFactory, new SSDFactory];
+
+foreach ($creators as $creator) {
+    $creator->createDisc()->readMemory();
+    echo  "<br />";
+}
