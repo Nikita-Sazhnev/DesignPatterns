49bd65dbd5531af849e5d9b1cc2a0456be782317
Nikita <nikitas4478@gmail.com>
2020-11-23 21:20:41 +0200

    Factory Method

--- FactoryMethod.php
+++ FactoryMethod.php
@@ -1,26 +1,36 @@
 <?
+namespace creational;
 
 abstract class DiscFactory {
 
-    abstract public function createDisc(): DiscInterface;
+    abstract public function factoryMethod(): DiscInterface;
+
+    public function someLogic($data) {
+        $product = $this->factoryMethod();
+
+        $fullDisc = $product->writeMemory($data);
+
+        return $fullDisc;
+    }
 }
 
 class HDDFactory extends DiscFactory {
 
-    public function createDisc(): DiscInterface {
+    public function factoryMethod(): DiscInterface {
         return new HDD;
     }
 }
 
 class SSDFactory extends DiscFactory {
 
-    public function createDisc(): DiscInterface {
+    public function factoryMethod(): DiscInterface {
         return new SSD;
     }
 }
 interface DiscInterface {
+
     public function readMemory();
-    public function writeMemory();
+    public function writeMemory($data);
 }
 
 
@@ -29,16 +39,17 @@ class HDD implements DiscInterface {
     public function readMemory() {
         echo "Read data from magnet discs";
     }
-    public function writeMemory() {
+    public function writeMemory($data) {
         echo "Write data to memory discs";
     }
 }
 
 class SSD implements DiscInterface {
+
     public function readMemory() {
         echo "Read data from magnet cells";
     }
-    public function writeMemory() {
+    public function writeMemory($data) {
         echo "Write data to memory cells";
     }
 }
@@ -46,6 +57,6 @@ class SSD implements DiscInterface {
 $creators = [new HDDFactory, new SSDFactory];
 
 foreach ($creators as $creator) {
-    $creator->createDisc()->readMemory();
+    $creator->factoryMethod()->readMemory();
     echo  "<br />";
 }
